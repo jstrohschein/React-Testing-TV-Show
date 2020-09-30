ec049fe9f04262015d37e0c8e0fef121a80152cb
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-09-29 22:44:37 -0400

    Episodes tests

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Episodes from './Episodes';
+
+
+const episodesFixture = [
+  {
+    id: 'id',
+    image: 'imageSrc'
+  }
+]
+
+
+test('Episodes renders', () => {
+  render(<Episodes episodes={[]}/>);
+})
+
+test('Episodes shows data when rerendered with new episodes prop', () => {
+
+  const { queryAllByTestId, rerender } = render(<Episodes episodes={ [] } />);
+
+  expect(queryAllByTestId('episode')).toStrictEqual([]);
+  expect(queryAllByTestId('episode')).toHaveLength(0);
+
+  rerender(<Episodes episodes={ episodesFixture } />);
+
+  expect(queryAllByTestId('episode')).toHaveLength(1);
+
+})
+
+
+
+
+
+
+
